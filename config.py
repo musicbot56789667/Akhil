e9b915d47426ecd91da24d0acb04e51d79543559
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-06-28 09:16:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/Raichu")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/Akhil")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/a85421dcaa998be15d475.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/387e2fa48b3aebbf62d38.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://graph.org/file/3c32fea17d158d3bef6cb.jpg",
+    "https://te.legra.ph/file/cfffcc7124ea6f9a37772.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://graph.org/file/3c32fea17d158d3bef6cb.jpg"
+            PING_IMG_URL = "https://te.legra.ph/file/cfffcc7124ea6f9a37772.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://telegra.ph/file/a85421dcaa998be15d475.jpg"
+            START_IMG_URL = "https://te.legra.ph/file/387e2fa48b3aebbf62d38.jpg"
